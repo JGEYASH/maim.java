91412e4487c47f92c5176962a3e53826edef42c5
JGEYASH <146904741+JGEYASH@users.noreply.github.com>
2023-10-04 08:16:44 +0530

    Add files via upload

---
+++ pankaj.py..py
@@ -0,0 +1,8 @@
+#Python program to read and print username.
+
+#take input from user
+fname = input("Enter your first name:")
+lname = input("Enter your last name:")
+#concatinating string("Your name is")with variable(name)
+#I have used Escape sequence character\t stands for tab.
+print("Your name is\t"+fname+lname)
